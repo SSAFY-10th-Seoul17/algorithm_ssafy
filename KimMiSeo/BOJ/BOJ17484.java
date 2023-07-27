b3357ab761296428af61229f11b85b4b333b7ba7
seomiii <86819719+seomiii@users.noreply.github.com>
2023-07-28 00:32:56 +0900

    [refactor] boj17484

--- KimMiSeo/BOJ/BOJ17484.java
+++ KimMiSeo/BOJ/BOJ17484.java
@@ -6,10 +6,11 @@ import java.io.*;
 public class BOJ17484 {
 	static int n,m;
 	static int graph[][];
-	static int min = Integer.MAX_VALUE;
+	static int min = 100*n*m;
 	
 	static int[] dy = {-1, 0, 1};
 	static int[] visited;
+//	static int result = 100*n*m;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +26,68 @@ public class BOJ17484 {
 			}
 		}
 		
+//		for (int i=0; i<m; i++) {
+//			visited = new int[n];
+//			visited[0] = i;
+//			dfs(1,i,-1);
+//		}
+//		
+//		System.out.println(min);
+		
+		//--------- dp ----------		
+		int result = solve(graph);
+		System.out.println(result);
+		
+	}
+	
+	private static int solve(int[][] graph) {
+		int[][][] dp = new int[n][m][3];
+		
+		// 첫 줄 초기화
 		for (int i=0; i<m; i++) {
-			visited = new int[n];
-			visited[0] = i;
-			dfs(1,i,-1);
+			dp[0][i][0] = graph[0][i];
+			dp[0][i][1] = graph[0][i];
+			dp[0][i][2] = graph[0][i];
+			
 		}
 		
-		System.out.println(min);
+		// dp 초기화
+		for (int i=1; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = 100*n*m;
+			}
+		}
 		
 		
+		for (int i=1; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				for(int k=0; k<3; k++) {
+					// 이전 좌표 구하기
+					int beforeX = i - 1;
+					int beforeY = j - dy[k];
+					
+					// 이전좌표, 다른방향에서 온 값 + 현재값 vs 현재 dp값
+					if (isValid(beforeX, beforeY)) {						
+						dp[i][j][k] = Math.min(dp[i][j][k], dp[beforeX][beforeY][(k+1)%3]+graph[i][j]);
+						dp[i][j][k] = Math.min(dp[i][j][k], dp[beforeX][beforeY][(k+2)%3]+graph[i][j]);
+					}
+				}
+			}
+		}
+		
+		int result = 100*n*m;
+		for (int i=0; i<m; i++) {
+			for (int j=0; j<3; j++) {
+				result = Math.min(result, dp[n-1][i][j]);
+			}
+		}
+		
+		return result;
+		
+	}
+	
+	private static boolean isValid(int x, int y) {
+		return 0<=x && x<n && 0<=y && y<m;
 	}
 	
 	public static void dfs(int i, int j, int dir) {
