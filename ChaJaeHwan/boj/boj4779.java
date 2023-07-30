fcd4d1d92727d5ac54c98f6c66f389afb2de9ac3
barded1998 <barded1998@gmail.com>
2023-07-30 15:37:06 +0900

    [solved] boj4799

---
+++ ChaJaeHwan/boj/boj4779.java
@@ -0,0 +1,38 @@
+package ChaJaeHwan.boj;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class boj4779 {
+
+    public static int n;
+    public static int length;
+    public static char[] chars;
+    public static String input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while ((input = br.readLine()) != null) {
+            n = Integer.parseInt(input);
+            length = (int) Math.pow(3, n);
+            chars = new char[length];
+            Arrays.fill(chars, '-');
+            cantor(0, length);
+            bw.write(chars);
+            bw.write('\n');
+            bw.flush();
+        }
+    }
+
+    public static void cantor(int start, int length) {
+        if (length == 1) {
+            return;
+        }
+        length /= 3;
+        Arrays.fill(chars, start + length, start + length * 2, ' ');
+        cantor(start, length);
+        cantor(start + length * 2, length);
+    }
+}
