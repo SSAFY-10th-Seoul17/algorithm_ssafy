2cd17814d644284aab26c6edf97efab0ebeea79d
leetag <leetag1163@naver.com>
2023-07-30 10:08:43 +0900

    [fix] boj14606

--- LeeTaeHo/boj/boj14606.java
+++ LeeTaeHo/boj/boj14606.java
@@ -5,9 +5,11 @@ public class boj14606 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] dp = new int[11];
-		dp[0] = dp[1] = 0;
-		for(int i = 2; i <= 10; i++) {
-			dp[i] = i-1 + dp[i-1];
+		dp[2] = 1;
+		for(int i = 3; i <= n; i++) {
+			int n1 = i / 2;
+			int n2 = i % 2 != 0 ? i / 2 + 1 : i / 2;
+			dp[i] = n1 * n2 + dp[n1] + dp[n2];
 		}
 		System.out.println(dp[n]);
 	}
