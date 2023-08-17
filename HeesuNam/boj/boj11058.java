e74c703ab55ed600b3359836b7fac3912e03a69b
heesu <skagmltn7@hanmail.net>
2023-08-17 09:42:10 +0900

    [solved] boj11058

--- HeesuNam/boj/boj11058.java
+++ HeesuNam/boj/boj11058.java
@@ -3,35 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	private static int ans = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		if (N <= 3) {
+		if (N <= 6) {
 			System.out.println(N);
 		} else {
-			click(3, 0, N - 3, 0);
-			System.out.println(ans);
-		}
-	}
+			long[] dp = new long[N + 1];
 
-	private static void click(int total, int stack, int n, int chk) {
-		if (n == 0) {
-			if (ans < total) {
-				ans = total;
+			for (int i = 1; i <= 6; i++) {
+				dp[i] = i;
+			} // 3개이상부터 3개 단위로 a(i-3),c(i-2),v(i-1)누름
+
+			for (int i = 7; i <= N; i++) {
+				dp[i] = dp[i - 1] + 1; // A출력
+				for (int j = 3; j < i; j++) {
+					dp[i] = Math.max(dp[i], dp[i - j] * (j - 1)); // acv다누르는경우, 두번 v누르는 경우, 세번 v누르는경우
+				}
 			}
-			return;
+			System.out.println(dp[N]);
 		}
-		click(total + 1, stack, n - 1, chk);// A누르는 경우
-		if (n - 3 >= 0) {
-			click(total, total, n - 1, 1);// ctrl a인 경우 chk=1바꾸기
-		}
-		if (chk == 1 && n - 2 >= 0) {
-			click(total, stack, n - 1, 2);// ctrl c인 경우 chk=2바꾸기
-		}
-		if (chk == 2) {
-			click(total + stack, stack, n - 1, chk);// ctrl v인 경우
-		}
-	}
-}
+	} // end of main
+} // end of class
