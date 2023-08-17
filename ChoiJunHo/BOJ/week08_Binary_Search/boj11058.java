96d4286668acad3314ba40bf54d959a2dd86f8e9
junhochoi-dev <cjh970422@gmail.com>
2023-08-17 19:36:24 +0900

    [solved] boj11058.java

---
+++ ChoiJunHo/BOJ/week08_Binary_Search/boj11058.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 1];
+        long[] ac = new long[N + 1];
+        for(int i = 0; i <= N; i++){
+            if(i >= 2) ac[i] = Math.max(i - 2, dp[i - 2]);
+            dp[i] = i;
+            for(int j = i - 1; j >= 2; j--) dp[i] = Math.max(dp[i], ac[j] * (i - j + 1));
+        }
+        System.out.println(dp[N]);
+    }
+}
