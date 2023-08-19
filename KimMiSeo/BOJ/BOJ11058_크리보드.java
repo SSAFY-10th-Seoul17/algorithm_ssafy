b10b0e134bdd1ec1a9caac63b03abe8612210e0b
seomiii <86819719+seomiii@users.noreply.github.com>
2023-08-19 11:48:30 +0900

    [resolved] boj11058

--- KimMiSeo/BOJ/BOJ11058_크리보드.java
+++ KimMiSeo/BOJ/BOJ11058_크리보드.java
@@ -2,12 +2,6 @@ import java.util.*;
 import java.io.*;
 // 📝 
 
-// 잘 모르겠어서 블로그 찾아봤습니다!
-// 문제를 읽고 dp로 풀어야겠다는 생각을 하긴했습니다. 
-// dp[i-3]*2, dp[i-4]*3 까지 밖에 생각하지 못했습니다. dp를 long 배열로 선언하는 것도 생각하지 못했던 것같습니다.
-
-// 테스트 케이스를 보지 않고 long으로 선언해야겠다는 판단을 어떻게 하는지 궁금합니다. 
-
 public class BOJ11058_크리보드 {
 	static int n;
 	static long[] dp;
@@ -18,15 +12,19 @@ public class BOJ11058_크리보드 {
 		n = Integer.parseInt(br.readLine());
 		dp = new long[n+1];
 		
-		for (int i=1; i<=n; i++) {
-			dp[i] = dp[i-1]+1;
-			
-			if (i>6) {
-				for (int j=2; j<5; j++) {
-					dp[i] = Math.max(dp[i], dp[i-(j+1)]*j);
-				}
-			}
+		// a출력
+		for (int i=0; i<=n; i++) {
+			dp[i] = i;
 		}
+		
+		for (int i=4; i<=n; i++) {
+			long buffer = dp[i-3]; // 복사해놓은 값
+			
+			for (int j=i, index=2; j<=n; j++, index++) { // i-3을 가지고 계속 복사하는 상황
+				dp[j] = Math.max(dp[j], dp[i-3]*index);
+			}
+		} 
+		
 		System.out.println(dp[n]);
 	}
 }
