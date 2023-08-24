4ef53093a8dcd72b4ef8fb173d1249739f3a0da3
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2023-08-25 08:44:25 +0900

    [solved] boj8394

---
+++ dpwns/boj/boj8394.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10;
+        }
+        System.out.println(dp[n]);
+        br.close();
+    }
+}
