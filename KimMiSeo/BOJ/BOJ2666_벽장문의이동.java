da7da0ae33344857057b1788f87c08a9751e836e
seomiii <rlaaltj1765@gmail.com>
2023-09-11 21:56:49 +0900

    [resolved] BOJ2666_벽장문의이동

--- KimMiSeo/BOJ/BOJ2666_벽장문의이동.java
+++ KimMiSeo/BOJ/BOJ2666_벽장문의이동.java
@@ -4,6 +4,7 @@ public class BOJ2666_벽장문의이동 {
     static int N,open1,open2,L,result; // 벽장의 개수, 열려있는 위치, 사용할 벽장 순서 길이
     static boolean[] doors; // 문
     static int[] play;
+    static int[][][] memo;
     public static void main(String[] args) throws Exception {
         // n개의 벽장 일렬, 문은 n-2개
         // 문이 없으면 앞으로 움직일 수 있음
@@ -18,6 +19,7 @@ public class BOJ2666_벽장문의이동 {
         L = Integer.parseInt(br.readLine());
         play = new int[L];
         result = Integer.MAX_VALUE;
+        memo = new int[L][N+1][N+1];
 
         doors = new boolean[N+1];
         Arrays.fill(doors, true);
@@ -27,27 +29,30 @@ public class BOJ2666_벽장문의이동 {
         for (int i=0; i<L; i++){
             play[i] = Integer.parseInt(br.readLine()); // 비어있어야 함
         }
-        solve(0,open1, open2, 0);
-
+        result = solve(0,open1, open2);
         System.out.println(result);
     }
-    private static void solve(int index, int open1, int open2, int cnt){
+    private static int solve(int index, int open1, int open2){
         if (index == L){
-            result = Math.min(cnt, result);
-            return;
+//            System.out.println(index+" "+open1+" "+open2);
+            return 0;
+        }else if (memo[index][open1][open2] > 0){
+            return memo[index][open1][open2];
         }
         // 첫번째 문
         int c = Math.abs(open1 - play[index]);
         doors[play[index]] = false; // 문 없음
         doors[open1] = true; // 문 있음
 
-        solve(index+1, play[index], open2,  cnt+c);
-
-        c = Math.abs(open2 - play[index]);
+        int cc = Math.abs(open2 - play[index]);
         doors[play[index]] = false;
         doors[open2] = true;
-        solve(index+1, open1, play[index], cnt+c);
 
+        return memo[index][open1][open2] =  Math.min(
+                c + solve(index+1, play[index], open2),
+                cc + solve(index+1, open1, play[index])
+        );
 
+//        return memo[index][open1][open2];
     }
 }
