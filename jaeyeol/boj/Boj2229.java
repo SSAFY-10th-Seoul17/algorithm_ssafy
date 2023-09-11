641053e7ca3dd8b35cc3d0384dc9e3fbab1317cf
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-09-11 19:33:48 +0900

    Update and rename No2229.java to Boj2229.java

--- jaeyeol/boj/Boj2229.java
+++ jaeyeol/boj/Boj2229.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class No2229 {
+public class Boj2229 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
