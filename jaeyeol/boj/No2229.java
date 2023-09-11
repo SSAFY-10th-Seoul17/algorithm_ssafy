76e70b382d4fd5a058f5d7f13f7aad96292b40dd
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-09-11 19:32:17 +0900

    Create No2229.java

---
+++ jaeyeol/boj/No2229.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class No2229 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(getMaxTeamScore(arr, n));
+    }
+
+    private static int getMaxTeamScore(int[] arr, int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+
+            for (int j = i; j > 0; j--) {
+                max = Math.max(max, arr[j]);
+                min = Math.min(min, arr[j]);
+
+                dp[i] = Math.max(dp[i], max - min + dp[j - 1]);
+            }
+        }
+
+        return dp[n];
+    }
+
+}
+
