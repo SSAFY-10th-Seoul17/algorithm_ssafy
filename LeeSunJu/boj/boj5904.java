5ddd2fbd3e042ff355ed1c5a14fdd0b043d38b55
sunju5402 <ssonju5402@gmail.com>
2023-09-14 20:34:59 +0900

    [solved] boj5904

---
+++ LeeSunJu/boj/boj5904.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj5904 {
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		int k = 0;
+		int length = 3;
+		while (length < N) {
+			length *= 2;
+			length += (++k + 3);
+		}
+
+		N -= 1;
+		System.out.println(solve(k, length));
+	}
+
+	private static char solve(int k, int n) {
+		int mid = (n - (k + 3)) / 2;
+
+		if (N >= n - mid) {
+			N = N - mid - (k + 3);
+
+		}
+		if (N >= mid && N < n - mid) {
+			if (N == mid) {
+				return 'm';
+			} else {
+				return 'o';
+			}
+		}
+
+		return solve(k - 1, mid);
+	}
+}
