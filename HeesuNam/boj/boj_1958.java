9e0489b154810cca492f4bc2bb6565ee3c7950b4
Heesu <133394749+skagmltn7@users.noreply.github.com>
2023-09-18 20:00:40 +0900

    남희수 / 9월 2주차 / 목  (#226)
    * init
    
    * weeks4 Monday
    
    * [solved] boj17616
    
    * [solved] boj_8394.java
    
    * [solved] boj_18243
    
    * [solved] boj_14567
    
    * [solved] boj_21608
    
    * [solved] boj_1958
    
    * [solved] boj_12761
    
    * [solved] boj_13265
    
    * [solved] boj_9084
    
    * [solved] boj_20007
    
    * [solved] boj_9024
    
    * Rename HeesuNam/boj_9024.java to HeesuNam/boj/boj_9024.java
    
    * [solved] boj_18428
    
    * [solved] boj16397
    
    * [solved] boj_5904
    
    * [solved] boj_1174
    
    * [solved] boj_3079
    
    * [solved] boj_1780
    
    * [solved] boj_2096
    
    * [solved] boj_3020

---
+++ HeesuNam/boj/boj_1958.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+
+	static int[][][] dp;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		String s3 = br.readLine();
+		
+		System.out.println(LCS3(s1,s2,s3));
+		
+	}
+	
+	static int LCS3(String str1, String str2, String str3) {
+		int n1 = str1.length();
+		int n2 = str2.length();
+		int n3 = str3.length();
+		dp = new int[n1+1][n2+1][n3+1];
+		
+		for(int i=1; i<n1+1; i++) {
+            char ac = str1.charAt(i-1);
+			for(int j=1; j<n2+1; j++) {
+                char bc = str2.charAt(j-1);
+				for(int k=1; k<n3+1; k++) {
+                    char cc = str3.charAt(k-1);
+					if(ac == bc && bc==cc){
+						dp[i][j][k] = dp[i-1][j-1][k-1] +1;
+					}else {
+						dp[i][j][k] = Math.max(dp[i-1][j][k],
+								Math.max(dp[i][j-1][k], dp[i][j][k-1]));
+					}
+				}
+			}
+		}
+		return dp[n1][n2][n3];
+	}
+}
